05ef12d654e86af0a0defd6bf92248739e4e863c
Samuel <colaco.samuel8@gmail.com>
2024-09-12 16:11:49 -0300

    Adicionando arquivos middlewares e rotas

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+
+
+export const routes = [
+    {
+        method: "GET",
+        path: "/tickets",
+        controller({ req, res }){
+        }
+    },
+
+    {
+        method: "POST",
+        path: "/tickets",
+        controller({ req, res }){
+        }
+    },
+
+    {
+        method: "PUT",
+        path: "/tickets/:id",
+        controller({ req, res }){
+        }
+    },
+
+    {
+        method: "PATCH",
+        path: "/tickets/:id/status",
+        controller({ req, res }){
+        }
+    },
+
+    {
+        method: "DELETE",
+        path: "/tickets/:id",
+        controller({ req, res }){
+        }
+    }
+]
